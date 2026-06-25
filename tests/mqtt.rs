3de07a4ec2a0ff52bbc3a36359b7f0a828815e4c
johnwhitton <john@johnwhitton.com>
2026-06-24 23:19:06 -0700

    refactor: organize MQTT integration tests

---
+++ tests/mqtt.rs
@@ -0,0 +1,11 @@
+#[path = "mqtt/adapter_tests.rs"]
+mod adapter_tests;
+
+#[path = "mqtt/client_tests.rs"]
+mod client_tests;
+
+#[path = "mqtt/topics_tests.rs"]
+mod topics_tests;
+
+#[path = "mqtt/transport_tests.rs"]
+mod transport_tests;
